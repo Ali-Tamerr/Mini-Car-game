cc4fe6e6399cd6cb479ee44b04cd5b40f210c4a4
Ali-Tamerr <extelx66@gmail.com>
2026-03-23 00:42:11 +0200

    Updates

--- _next/static/UTYbwqqvyMnt-QpZnxfYK/_buildManifest.js
+++ _next/static/UTYbwqqvyMnt-QpZnxfYK/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Mini-Car-game//_next/:path+",
+        "destination": "/Mini-Car-game/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
